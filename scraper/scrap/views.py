022304fc67617f2aca4a79391c2a68f9c073145c
pranishch <pranishchaudhary520@gmail.com>
2025-03-02 19:40:04 +0545

    Second commit

--- scraper/scrap/views.py
+++ scraper/scrap/views.py
@@ -6,7 +6,7 @@ import os
 from .models import StockData
 
 def stock_list(request):
-    """Fetch all stock data from the database and render it in the template."""
+    """Fetch all stock data from the  database and render it in the template."""
     stocks = StockData.objects.all()  # Fetch latest data first
     # stocks = StockData.objects.all().order_by('serial_number')  # Ensure ascending order
     return render(request, 'csv_template.html', {'stocks': stocks})
