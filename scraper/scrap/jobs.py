220ae68db1f8111bfc91c6cc9cceaf509e446d14
pranishch <pranishchaudhary520@gmail.com>
2025-03-06 00:52:43 +0545

    success

--- scraper/scrap/jobs.py
+++ scraper/scrap/jobs.py
@@ -1,8 +1,11 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .stock_scraper import scrape_nepse_data
-from apscheduler.triggers.interval import IntervalTrigger
+import time
+
 def start_jobs():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(scrape_nepse_data)  # Runs every 5 minutes
+    # Schedule the job to run 
+    scheduler.add_job(scrape_nepse_data)
     scheduler.start()
     print("Scheduler started")
+
