ad57bc019d6f22e3490a6ccb7ae794f2c4fe7602
pranishch <pranishchaudhary520@gmail.com>
2025-03-06 17:59:44 +0545

    change

--- scraper/scrap/urls.py
+++ scraper/scrap/urls.py
@@ -3,5 +3,5 @@ from .views import stock_list, stock_FS
 
 urlpatterns = [
     path('stocks/', stock_list, name='stock_prices'),
-    path('Floor/', stock_FS, name='stock_sheet'),
+    path('floor/', stock_FS, name='stock_sheet'),
 ]
